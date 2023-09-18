c2b2ffb7fe54c601e07791e6cf123bf119101355
Ben Shutt <shuttbenjamin@gmail.com>
2023-09-18 18:34:23 +0100

    Initial Commit

---
+++ MediaExport/MediaExportApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaExportApp.swift
+//  MediaExport
+//
+//  Created by Ben Shutt on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MediaExportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
